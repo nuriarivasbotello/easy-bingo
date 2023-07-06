32768dca7ecb1fd1d72407029e7c71bf72135cba
nuriarivasbotello <nuriarivasbotello@gmail.com>
2023-07-06 16:32:18 +0200

    +

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+const userBingoElement = document.getElementById('user-bingo');
+const pcBingoElement = document.getElementById('pc-bingo');
+const numbersBingo = document.getElementById('numbers');
+const buttonElement = document.getElementById('button');
+let userPlay;
+const buttonBack = event => {
+  userPlay = event.target.dataset.button;
+};
